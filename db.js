01980eb80dbd39d96ca143f976a936148c2a90da
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-01-01 07:33:21 +0530

    events

---
+++ db.js
@@ -0,0 +1,48 @@
+
+// create a generic function to execute queries and return the results in a promise (so we can use async/await) 
+// and then use it in the routes instead of the code above (which is repeated for each route) 
+
+import mysql from 'mysql';
+import { MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER } from './config.js';
+let pool;
+
+try {
+    console.log("Connecting to SQL", MYSQL_DATABASE, MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD)
+    pool = mysql.createPool({
+        connectionLimit: 10,
+        host: MYSQL_HOST,
+        database: MYSQL_DATABASE,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD
+    });
+    console.log("Connection Success")
+}
+catch (error) {
+    console.log("SQL Error", error)
+}
+
+export function executeQuery(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                console.log("Error", err);
+                if (connection) connection.release();
+                reject(err);
+                return;
+            }
+
+            connection.query(sql, params, (err, results) => {
+                if (err) {
+                    console.log("Error", err);
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+                connection.release();
+            });
+        });
+    });
+}
+
+
+export { pool };
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import cors from 'cors';
 import express from 'express';
+import { executeQuery } from './db.js';
+import { log } from 'console';
+
 
 const app = express();
 const port = 8080;
@@ -8,12 +11,12 @@ app.use(cors());
 
 const clients = {};
 
-app.get('/events', (req, res) => {
+app.get('/sse', (req, res) => {
 	const clientId = Date.now();
 	console.log(`Client ${clientId} connected`);
 
 	res.setHeader('Content-Type', 'text/event-stream');
-	res.setHeader('Cache-Control', 'no-cache');
+	// res.setHeader('Cache-Control', 'no-cache');
 
 	clients[clientId] = res;
 
@@ -23,20 +26,38 @@ app.get('/events', (req, res) => {
 	})
 })
 
-function sendToClients(event, data) {
+
+function sendToClients(data) {
 	Object.keys(clients).forEach((clientId) => {
-		clients[clientId].write(`event: ${event}\ndata: ${JSON.stringify(data)}\n\n`);
+		clients[clientId].write(`data: ${JSON.stringify(data)}\n\n`);
 	});
 }
 
 // send a random number every 2 seconds to the client 
 // send this number to all the clients connected to the server
+app.get('/ping', (req, res) => {
+	console.log(`Sending ${randomNumber}`);
+	sendToClients('ping', "Hello")
+	res.status(200).send('OK');
+})
+
+app.get('/sample', (req, res) => {
+	const query = `SELECT Title, Tags, Region, City, City, Country , lat, lng, RecordKey FROM Records LIMIT 100;`
+	executeQuery(query)
+		.then((data) => {
+			res.status(200).json(data);
+		}).catch((rsn) => {
+			log("Error", rsn)
+			res.status(500).json({ error: rsn })
+		})
+})
+
 
 function startSendingRandomNumbers() {
 	setInterval(() => {
 		const randomNumber = Math.floor(Math.random() * (7000 - 700 + 1) + 700);
 		console.log(`Sending ${randomNumber}`);
-		sendToClients('ping', randomNumber);
+		sendToClients(randomNumber);
 	}, 4000);
 }
 
