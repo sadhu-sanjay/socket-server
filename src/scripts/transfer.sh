da00a1552be981a496b62196d3f357e52a280f4c
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-01-10 19:21:46 +0530

    updated transfer script

--- src/scripts/transfer.sh
+++ src/scripts/transfer.sh
@@ -3,11 +3,11 @@
 
 USER="ec2-user"
 SERVER_ADDRESS="ec2-3-7-72-9.ap-south-1.compute.amazonaws.com"
-RSYNC_PRIVATE_KEY="-e ssh -i ./key/airtable-maps-test.pem"
-PRIVATE_KEY="./key/airtable-maps-test.pem"
+RSYNC_PRIVATE_KEY="-e ssh -i ./src/keys/aws-test-server.pem"
+PRIVATE_KEY="./src/keys/aws-test-server.pem"
 
 SOURCE_DIR="./"
-EXCLUDE_FILE="--exclude node_modules --exclude .git --exclude --exclude key"
+EXCLUDE_FILE="--exclude node_modules --exclude .git --exclude --exclude src/keys"
 DESTINATION_DIR="~/socket-server"
 
 # transfer the files to the remote machine
@@ -17,6 +17,8 @@ rsync -avz $EXCLUDE_FILE "$RSYNC_PRIVATE_KEY" $SOURCE_DIR $USER@$SERVER_ADDRESS:
 ssh -i $PRIVATE_KEY $USER@$SERVER_ADDRESS <<EOF
     cd $DESTINATION_DIR
     npm install
-    sudo service socket-server restart
+    sudo cp src/scripts/socket-server.service /etc/systemd/system/socket-server.service
+    sudo systemctl daemon-reload
+    sudo systemctl restart socket-server
 EOF
 
